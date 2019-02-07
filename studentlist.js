a9925d383fef2523216cfcfbeca4530ca88a670f
Peter Lind <petl@kea.dk>
2019-02-07 12:47:15 +0100

    added some scaffolding

--- studentlist.js
+++ studentlist.js
@@ -6,5 +6,32 @@ function init() {
   console.log("init");
 
   // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
+  getJSON();
+}
+
+function getJSON() {
+  console.log("getJSON");
+
+  // NOTE: Maybe also call sortByFirst the first time ... Investigate!
+  filterList();
+}
+
+function filterList() {
+  console.log("filterList");
+
+  displayList();
+}
+
+function displayList() {
+  console.log("displayList");
+}
+
+// TODO: Create scaffolding functions for the rest!
+
+function clickSortByFirst() {
+
+}
+
+function sortListByFirst() {
 
 }

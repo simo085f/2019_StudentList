7d08dd412a8c9a52a1da44bc7ba7cee1073f5ced
Peter Lind <petl@kea.dk>
2019-02-07 01:31:41 +0100

    first commit of almost empty files

---
+++ studentlist.js
@@ -0,0 +1,10 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  console.log("init");
+
+  // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
+
+}
